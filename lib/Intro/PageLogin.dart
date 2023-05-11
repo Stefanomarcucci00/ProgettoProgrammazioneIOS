770430b1c18c29cde3e88ba5d5ae7ed18ddc741e
MattSona99 <matteo.sonaglioni@gmail.com>
2023-05-11 18:10:33 +0200

    Login funzionante ma non carica un cazzo

--- lib/Intro/PageLogin.dart
+++ lib/Intro/PageLogin.dart
@@ -1,36 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:progetto_programmazione_ios/PageRistoranti.dart';
 
 class PageLogin extends StatelessWidget {
-  static Future<User?> loginUsingEmailPassword(
-      {required String email,
-      required String password,
-      required BuildContext context}) async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user;
+  const PageLogin({super.key});
+
+  Future<void> loginUser(
+      String email, String password, BuildContext context) async {
     try {
-      UserCredential userCredential = await auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      user = userCredential.user;
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      User? user = userCredential.user;
+      Fluttertoast.showToast(
+          msg: "Login effettuato con successo.",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PageRistoranti(user: user,)),
+      );
     } on FirebaseAuthException catch (e) {
-      if (e.code == "user-not-found") {
-        print("Nessun utente registrato con queste credenziali");
+      if (e.code == 'user-not-found') {
+        Fluttertoast.showToast(
+            msg: "Utente non registrato.",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      } else if (e.code == 'wrong-password') {
+        Fluttertoast.showToast(
+            msg: "Password errata.",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
       }
     }
-    return user;
   }
 
   @override
   Widget build(BuildContext context) {
-
-    TextEditingController _emailController = TextEditingController();
-    TextEditingController _pwdController = TextEditingController();
+    TextEditingController emailController = TextEditingController();
+    TextEditingController pwdController = TextEditingController();
 
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/background_splash.jpg'),
             fit: BoxFit.cover,
@@ -40,38 +64,31 @@ class PageLogin extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextFormField(
-              controller: _emailController,
+              controller: emailController,
               keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Email',
               ),
             ),
             TextFormField(
-              controller: _pwdController,
-              decoration: InputDecoration(
+              controller: pwdController,
+              decoration: const InputDecoration(
                 labelText: 'Password',
               ),
               obscureText: true,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                User? user = await loginUsingEmailPassword(
-                    email: _emailController.text,
-                    password: _pwdController.text,
-                    context: context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PageRistoranti()),
-                );
+                loginUser(emailController.text, pwdController.text, context);
               },
-              child: Text('Entra', style: TextStyle(color: Colors.white)),
               style: ElevatedButton.styleFrom(
                 primary: Colors.red,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30),
                 ),
               ),
+              child: const Text('Entra', style: TextStyle(color: Colors.white)),
             ),
           ],
         ),

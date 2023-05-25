fba33bf7e26cfaf7367935264610adf9efad0f3a
Stefanomarcucci00 <marcuccistefano00@gmail.com>
2023-05-25 16:00:09 +0200

    grafica

---
+++ lib/viewmodels/CartViewModel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CartViewModel extends StatefulWidget {
+  const CartViewModel({Key? key}) : super(key: key);
+
+  @override
+  State<CartViewModel> createState() => _CartViewModelState();
+}
+
+class _CartViewModelState extends State<CartViewModel> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
